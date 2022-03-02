a5a518701ed55813728f22de55b3fc04efb40f03
Samantha Rexroat <samanthajrexroat@gmail.com>
2022-03-02 16:51:52 -0700

    Installed inquirer and fs module

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title',
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -12,8 +12,12 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
+  return `
+    # ${data.title}
+    ## Description 
+    > ${data.description}
+    ## Installation
+    
 `;
 }
 
